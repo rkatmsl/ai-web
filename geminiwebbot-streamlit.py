ec90c121e454355cefa13bbb58552682d31d09e1
rkatmsl <153056657+rkatmsl@users.noreply.github.com>
2025-04-10 21:54:27 +0530

    Update geminiwebbot-streamlit.py

--- geminiwebbot-streamlit.py
+++ geminiwebbot-streamlit.py
@@ -9,7 +9,7 @@ import time
 import os
 
 pg_pass = st.secrets["PG_PASS"]
-db_url = f"postgresql+psycopg2://postgres:{pg_pass}@database-2.czg44aga0cfb.ap-south-1.rds.amazonaws.com:5432/ai"
+db_url = f"postgresql+psycopg2://postgres:{pg_pass}@database-1.cluster-czg44aga0cfb.ap-south-1.rds.amazonaws.com:5432/ai"
 
 # db_url = "postgresql+psycopg://ai:ai@localhost:5432/ai"
 # knowledge_base.load(recreate=True)  # Comment out after first run
